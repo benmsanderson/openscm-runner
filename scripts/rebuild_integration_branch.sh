2c55d947249b6c2e85ac0f6c7f109a620542b91e
Ben Sanderson <ben.m.sanderson@gmail.com>
2026-05-23 17:09:44 +0200

    Add modernisation/fair2-conc-driven to integration rebuild list

--- scripts/rebuild_integration_branch.sh
+++ scripts/rebuild_integration_branch.sh
@@ -33,6 +33,7 @@ BRANCHES=(
   modernisation/netcdf-writer
   modernisation/fair2-adapter
   modernisation/ciceroscmpy2-adapter
+  modernisation/fair2-conc-driven
 )
 
 INTEGRATION_BRANCH="modernisation/integration"
