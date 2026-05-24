6d48fb73dfa6cdae9b3eae59144d2d979b957b0f
Ben Sanderson <ben.m.sanderson@gmail.com>
2026-05-24 20:27:43 +0200

    Sync jupytext header for compare_rcmip3.py

--- notebooks/compare_rcmip3.py
+++ notebooks/compare_rcmip3.py
@@ -2,12 +2,13 @@
 # ---
 # jupyter:
 #   jupytext:
+#     custom_cell_magics: kql
 #     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.16.0
+#       jupytext_version: 1.11.2
 #   kernelspec:
 #     display_name: .venv
 #     language: python
